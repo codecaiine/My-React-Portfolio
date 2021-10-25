d4e9d4f5eeb45bbd083a73e77784bb1c6bfd9637
codecaiine <akabrouyannickn@gmail.com>
2021-10-25 22:42:55 +0000

    Create components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3> My React Portfolio</h3>
-      </header>
+      <h3> My React Portfolio</h3>
     </div>
   );
 }
