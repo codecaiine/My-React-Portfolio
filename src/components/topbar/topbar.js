4e3b995deb23d57336d07cc1e1225ca0ecd452c4
codecaiine <akabrouyannickn@gmail.com>
2021-10-29 22:52:06 +0000

    Import material ui icons

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,4 +1,5 @@
 import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
@@ -7,7 +8,12 @@ export default function Topbar() {
         <div className="left">
           <a href="#introduction" className="logo">CoDecaine</a>
           <div className="itemContainer">
-            
+            <Person />
+            <span>+225 0709981789</span>
+          </div>
+          <div className="itemContainer">
+            <Mail />
+            <span>akabrouyannickn@gmail.com</span>
           </div>
         </div>
         <div className="right">
