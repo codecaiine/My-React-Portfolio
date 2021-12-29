939177026bd6526852e6249795713c8a336d3900
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-29 22:45:31 +0000

    Remove space in Menu.js

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,25 +1,25 @@
-import "./menu.scss";
+import './menu.scss';
 
-export default function Menu(){
-    return (
-         <div className="menu">
-              <ul>
-                  <li>
-                      <a href="#intro">Home</a>
-                  </li>
-                  <li>
-                      <a href="#portfolio">Portfolio</a>
-                  </li>
-                  <li>
-                      <a href="#works">Works</a>
-                  </li>
-                  <li>
-                      <a href="#testimonials">Testimonials</a>
-                  </li>
-                  <li>
-                      <a href="#contact">Contact</a>
-                  </li>
-              </ul>
-         </div>
-    )
+export default function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li>
+          <a href="#intro">Home</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#works">Works</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
