83d8d46e71c8207d8d5f36d75f8332f406a8c0c8
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-29 22:35:00 +0000

    Create menu in Menu.js

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,7 +3,23 @@ import "./menu.scss";
 export default function Menu(){
     return (
          <div className="menu">
-
+              <ul>
+                  <li>
+                      <a href="#intro">Home</a>
+                  </li>
+                  <li>
+                      <a href="#portfolio">Portfolio</a>
+                  </li>
+                  <li>
+                      <a href="#works">Works</a>
+                  </li>
+                  <li>
+                      <a href="#testimonials">Testimonials</a>
+                  </li>
+                  <li>
+                      <a href="#contact">Contact</a>
+                  </li>
+              </ul>
          </div>
     )
 }
