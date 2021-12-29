2f5d241abb5db57bc0177587e56f2f55f0a4927a
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-29 22:37:14 +0000

    Import Menu in App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
 import Topbar from './components/topbar/Topbar';
 import Works from './components/works/Works';
+import Menu from './components/menu/Menu';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="App">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu />
       <div className="sections">
         <Introduction />
         <Portfolio />
