6e9418814668aca35a430613e4c96877ac9427c7
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-30 20:07:07 +0000

    Add profile image to intro section

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -3,7 +3,11 @@ import './introduction.scss';
 export default function Introduction() {
   return (
     <div className="introduction" id="introduction">
-         <div className="left"></div>
+         <div className="left">
+             <div className="imgProfile">
+                  <img src="assets/pictures/profile.jpg" alt="Profile"/>
+             </div>
+         </div>
          <div className="right"></div>
     </div>
   );
