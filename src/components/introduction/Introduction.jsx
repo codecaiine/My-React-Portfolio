9606e28b378f41bd60c56fef108fda719005d4a4
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-30 20:58:16 +0000

    Add basic introduction with name

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -8,7 +8,16 @@ export default function Introduction() {
                   <img src="assets/pictures/profile.png" alt="Profile"/>
              </div>
          </div>
-         <div className="right"></div>
+         <div className="right">
+            <div className="wrapper">
+                 <h2> Hello My name is</h2>
+                 <h1>Yannick Noel</h1>
+                 <h3>Software Engineer<span></span></h3>
+            </div>
+            <a href="#portfolio">
+               <img src="assets/icons/arrow-down.png" alt="arrow-down"/>
+            </a>
+         </div>
     </div>
   );
 }
