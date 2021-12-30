04b245686aa704fd81becaf82edc5c9f08e7d88a
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-30 19:59:03 +0000

    Divide introduction page into left and right

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -2,6 +2,9 @@ import './introduction.scss';
 
 export default function Introduction() {
   return (
-    <div className="introduction" id="introduction" />
+    <div className="introduction" id="introduction">
+         <div className="left"></div>
+         <div className="right"></div>
+    </div>
   );
 }
