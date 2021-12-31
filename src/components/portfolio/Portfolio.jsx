26407b1c98990b43c82ccfdbeb14108f6b170f0c
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-31 00:50:11 +0000

    Add my work list

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,15 @@ import './portfolio.scss';
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio" />
+    <div className="portfolio" id="portfolio">
+        <h1>Portfolio</h1>
+        <ul>
+            <li>Featured</li>
+            <li>Web App</li>
+            <li>Mobile App</li>
+            <li>Design</li>
+            <li>Branding</li>
+        </ul>
+    </div>
   );
 }
