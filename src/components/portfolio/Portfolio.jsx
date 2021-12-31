3cd1c097f197cc0d126b672b105fa40d0f02f179
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2021-12-31 00:58:13 +0000

    Add work item structure

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,12 +5,19 @@ export default function Portfolio() {
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
-            <li>Featured</li>
-            <li>Web App</li>
-            <li>Mobile App</li>
-            <li>Design</li>
-            <li>Branding</li>
+            <li>All</li>
+            <li>Ruby</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>Java</li>
+            <li>JavaScript</li>
         </ul>
+        <div className="container">
+           <div className="work-item">
+             <img src="assets/social/coding.png" alt="my-work" />
+             <h3>My blog App</h3>
+           </div>
+        </div>
     </div>
   );
 }
