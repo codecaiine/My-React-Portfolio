92480934aa63bfda8f7f9ec04bed1348a829fc09
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 15:17:09 +0000

    Add basic structure to testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,15 @@ import './testimonials.scss';
 
 export default function Testimonials() {
   return (
-    <div className="testimonials" id="testimonials" />
+    <div className="testimonials" id="testimonials">
+     <h1>Testimonials</h1>
+     <div className="container">
+       <div className="card">
+         <div className="top"></div>
+         <div className="center"></div>
+         <div className="bottom"></div>
+       </div>
+     </div>
+    </div>
   );
 }
