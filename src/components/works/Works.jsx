6c9389fd3cf2e06ab33a0bc32158717d9f9eb1e9
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 13:22:32 +0000

    Add fake data

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,37 @@
 import './works.scss';
 
 export default function Works() {
+  
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
   return (
     <div className="works" id="works">
         <div className="slider">
