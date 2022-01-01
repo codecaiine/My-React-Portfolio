f2572093a54469845c3bfc4018aaccef6395eb9e
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 23:50:19 +0000

    Add function to handle form message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import './contact.scss';
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -8,10 +14,11 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form>
-            <input type="text" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <button type="submit">Send</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
       </div>
     </div>
