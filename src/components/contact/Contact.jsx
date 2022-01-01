45f9e1fa0b2218cedb10366de17a6fc890f25394
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 22:05:39 +0000

    Add background to form section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,12 @@ import './contact.scss';
 
 export default function Contact() {
   return (
-    <div className="contact" id="contact" />
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="img" />
+      </div>
+      <div className="right">
+      </div>
+    </div>
   );
 }
