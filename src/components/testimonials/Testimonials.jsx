e875e918f5d2de86b14e35a7f1257b3b0d0f1d81
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 21:14:16 +0000

    Add company logo

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -9,6 +9,7 @@ export default function Testimonials() {
       img:
         "https://media-exp1.licdn.com/dms/image/C4E03AQFHnfsZjxq7kg/profile-displayphoto-shrink_200_200/0/1635046570825?e=1646265600&v=beta&t=orWBagvNvGCdWjuDi-mVWv1EKteiV7Ahx_BE7LxR03Y",
       icon: "assets/social/twitter.png",
+      logo:"https://media-exp1.licdn.com/dms/image/C560BAQHr8P7gQ95yCQ/company-logo_200_200/0/1578673850004?e=1649289600&v=beta&t=4w64Xmx0_AZ3hIW9CkP6uBjYyy7To1BdinaWD2oaJ74",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
     },
@@ -19,6 +20,7 @@ export default function Testimonials() {
       img:
         "https://media-exp1.licdn.com/dms/image/D5635AQEMVA0zFCntEQ/profile-framedphoto-shrink_200_200/0/1634289246534?e=1641157200&v=beta&t=5KAt6SYpBK095yJE3AFG_R7ofaQwInG3GFo-nQ_bOtE",
       icon: "assets/social/twitter.png",
+      logo:"https://media-exp1.licdn.com/dms/image/C560BAQHr8P7gQ95yCQ/company-logo_200_200/0/1578673850004?e=1649289600&v=beta&t=4w64Xmx0_AZ3hIW9CkP6uBjYyy7To1BdinaWD2oaJ74",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
       featured: true,
@@ -30,6 +32,7 @@ export default function Testimonials() {
       img:
         "https://media-exp1.licdn.com/dms/image/C4E03AQG5Fj6jthO_7g/profile-displayphoto-shrink_200_200/0/1634589222858?e=1646265600&v=beta&t=kvpgjX7AevochTytbrFcD6Ol7HWzb-OzzGl8Q5zP05M",
       icon: "assets/social/linkedin.png",
+      logo:"https://media-exp1.licdn.com/dms/image/C560BAQHr8P7gQ95yCQ/company-logo_200_200/0/1578673850004?e=1649289600&v=beta&t=4w64Xmx0_AZ3hIW9CkP6uBjYyy7To1BdinaWD2oaJ74",
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
     },
@@ -41,7 +44,7 @@ export default function Testimonials() {
         {data.map((d) => (
           <div className={d.featured ? "card featured" : "card"}>
             <div className="top">
-              <img src="assets/right-arrow.png" className="left" alt="pic" />
+              <img src={d.logo} className="left" alt="logo" />
               <img
                 className="user"
                 src={d.img}
