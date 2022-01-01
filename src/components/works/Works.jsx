20d94a40413f145f350ca7d2fb4ab3913b3e1247
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 11:04:54 +0000

    Add image to left container

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -6,7 +6,13 @@ export default function Works() {
         <div className="slider">
           <div className="container">
             <div className="item">
-              <div className="left"></div>
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src="assets/social/coding.png" alt="work-one" />
+                  </div>
+                </div>
+              </div>
               <div className="right"></div>
             </div>
           </div>
