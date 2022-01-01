0ba3b5e14798561202a4398e691978da593f5d34
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-01 10:45:03 +0000

    Add basic structure into works component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,12 @@ export default function Works() {
   return (
     <div className="works" id="works">
         <div className="slider">
-          
+          <div className="container">
+            <div className="item">
+              <div className="left"></div>
+              <div className="right"></div>
+            </div>
+          </div>
         </div>
     </div>
   );
