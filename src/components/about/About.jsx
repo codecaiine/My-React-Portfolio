8f65637f8f244465bd21a24ca37fb51f07de669c
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-02 17:54:43 +0000

    Add basic structure to about component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="about" id="about">
+            <div className="left">
+
+            </div>
+            <div className="right">
+                
+            </div>
+        </div>
+    )
+}
