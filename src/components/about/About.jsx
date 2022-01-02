93052b7fcdc769ff4802071fdf3dc799ea877831
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-02 20:42:47 +0000

    Add content to about  component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,23 @@
 import './about.scss';
 
-export default function About() {
+const About = () => {
     return (
         <div className="about" id="about">
             <div className="left">
+                <div className="leftContainer">
+                    <h1>About Me</h1>
+                    <p>
+
+                    My name is Yannick and, I’m a Software Engineer and IT auditor. I have more than Five years of work experience in Software development, three years in Telcom, Startup, Banking Industries, and three years in IT Consultant. Also, I am a Tech Speaker with more than twenty talks in five countries. I have a passion for community building, pair programming, and remote work.
+                    I am open to job opportunities.
+
+                    I spend my free time reading articles and watching videos of world civilization. I like to listen to music.
+                    </p>
+                    <p>
+                    Are you looking for a developer? 
+                    <a href="#contact"> Let's schedule a call or a chat.</a>
+                    </p>
+                </div>
 
             </div>
             <div className="right">
@@ -12,3 +26,5 @@ export default function About() {
         </div>
     )
 }
+
+export default About;
