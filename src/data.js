d316e8df60edca4821cc17c88185b5481c3e0058
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-14 13:12:21 +0000

    Update my project

--- src/data.js
+++ src/data.js
@@ -1,26 +1,26 @@
 export const allPortfolio = [{
         id: 1,
-        title: "My project one",
-        url: "",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        title: "Bookstore",
+        url: "https://github.com/codecaiine/bookstore-react-redux",
+        img: "https://raw.githubusercontent.com/codecaiine/bookstore-react-redux/main/public/images/demo.PNG",
     },
     {
         id: 2,
-        title: "My project two",
-        url: "",
-        img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+        title: "Space Travelers",
+        url: "https://github.com/codecaiine/space-travelers-hub",
+        img: "https://raw.githubusercontent.com/codecaiine/space-travelers-hub/main/src/assets/img/capstone1.PNG",
     },
     {
         id: 3,
-        title: "My project three",
-        url: "",
-        img: "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+        title: "Catalog of things",
+        url: "https://github.com/codecaiine/catalog-of-my-things",
+        img: "https://s3.amazonaws.com/poly-screenshots.angel.co/enhanced_screenshots/2584700-original.",
     },
     {
         id: 4,
-        title: "My project four",
-        url: "",
-        img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+        title: "COVID Metrics app",
+        url: "https://github.com/codecaiine/metrics-app-covid19",
+        img: "https://raw.githubusercontent.com/codecaiine/metrics-app-covid19/main/src/assets/images/covid1.PNG",
     },
     {
         id: 5,
@@ -101,10 +101,11 @@ export const djangoPortfolio = [{
 ];
 
 export const reactPortfolio = [{
-        id: 1,
-        title: "Design Social Media App",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
+    id: 1,
+    title: "Bookstore",
+    url: "https://github.com/codecaiine/bookstore-react-redux",
+    img: "https://raw.githubusercontent.com/codecaiine/bookstore-react-redux/main/public/images/demo.PNG",
+},
     {
         id: 2,
         title: "Design Rampa UI Design",
