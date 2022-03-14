d7cbaeac8c1e73506698a7870ad0d928b2a909ae
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-14 12:37:48 +0000

    Add url item

--- src/data.js
+++ src/data.js
@@ -1,31 +1,37 @@
 export const allPortfolio = [{
         id: 1,
         title: "My project one",
+        url: "",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
         id: 2,
         title: "My project two",
+        url: "",
         img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
     },
     {
         id: 3,
         title: "My project three",
+        url: "",
         img: "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
     },
     {
         id: 4,
         title: "My project four",
+        url: "",
         img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
     },
     {
         id: 5,
         title: "My project five",
+        url: "",
         img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
     },
     {
         id: 6,
         title: "My project six",
+        url: "",
         img: "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
     },
 ];
